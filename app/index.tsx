f7ea610c283205fd4d89cb433f865ddfeb4b3217
Countyy <joaopedr1234567@gmail.com>
2024-06-03 20:46:55 -0300

    move app header to a separate component

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
-import Icon from 'react-native-vector-icons/AntDesign'
+import { FlatList, Text, TouchableOpacity, View } from 'react-native'
 import { NewTransaction } from '@/components/new-transaction'
+import { Header } from '@/components/header'
 
 export default function Index() {
   const [modalVisible, setModalVisible] = useState(false)
@@ -13,46 +13,49 @@ export default function Index() {
         setModalVisible={setModalVisible}
       />
 
-      <View className="bg-background flex-1 px-8 py-12">
-        <View className="flex flex-row justify-between mb-4 items-center">
-          <Text className="text-white text-4xl font-semibold">
-            Balanço Mensal
-          </Text>
+      <View className="bg-background flex-1 px-4 pt-20 flex gap-y-8">
+        <Header setModalVisible={setModalVisible} />
 
-          <TouchableOpacity onPress={() => setModalVisible(true)}>
-            <Icon
-              name="pluscircle"
-              color={'white'}
-              className="bg-red-400"
-              size={28}
-            />
-          </TouchableOpacity>
-        </View>
-
-        <View className="flex flex-row items-center gap-0 mb-2">
-          <View className="flex flex-row items-center">
-            <Icon name="upcircle" size={20} color={'green'} />
-            <Text className="text-green-600 text-xl"> R$350,00</Text>
+        <View className="flex-1 space-y-4">
+          <View className="py-3 px-4 border-l border-l-yellow-500">
+            <View className="flex flex-row justify-between">
+              <Text className="text-white">Compra no cartão de crédito</Text>
+              <Text className="text-white/30">Há 2 horas</Text>
+            </View>
+            <Text className="text-white text-lg">R$ 100,00</Text>
           </View>
 
-          <View className="flex flex-row items-center gap-2">
-            <View className="flex flex-row items-center">
-              <Icon name="upcircle" size={12} color={'green'} />
-              <Text className="text-green-300 text-xs"> R$400,00</Text>
+          <View className="py-3 px-4 border-l border-l-emerald-500">
+            <View className="flex flex-row justify-between">
+              <Text className="text-white">Recebimento</Text>
+              <Text className="text-white/30">Há 3 horas</Text>
             </View>
-
-            <View className="flex flex-row items-center">
-              <Icon name="downcircle" size={12} color={'red'} />
-              <Text className="text-red-300 text-xs"> R$50,00</Text>
-            </View>
+            <Text className="text-white text-lg">R$ 150,00</Text>
           </View>
-        </View>
 
-        <View className="flex flex-row gap-2 items-center">
-          <Icon name="creditcard" size={24} color={'white'} />
-          <Text className="text-white text-base">
-            Fatura do cartão: R$238,90
-          </Text>
+          <View className="py-3 px-4 border-l border-l-red-500">
+            <View className="flex flex-row justify-between">
+              <Text className="text-white">Compra no cartão de débito</Text>
+              <Text className="text-white/30">Há 1 mês</Text>
+            </View>
+            <Text className="text-white text-lg">R$ 150,00</Text>
+          </View>
+
+          <View className="py-3 px-4 border-l border-l-orange-400">
+            <View className="flex flex-row justify-between">
+              <Text className="text-white">Compra em dinheiro</Text>
+              <Text className="text-white/30">Há 1 ano</Text>
+            </View>
+            <Text className="text-white text-lg">R$ 150,00</Text>
+          </View>
+
+          <View className="py-3 px-4 border-l border-l-blue-500">
+            <View className="flex flex-row justify-between">
+              <Text className="text-white">Compra no PIX</Text>
+              <Text className="text-white/30">Há 1 ano</Text>
+            </View>
+            <Text className="text-white text-lg">R$ 150,00</Text>
+          </View>
         </View>
       </View>
     </>
---
+++ components/header.tsx
@@ -0,0 +1,46 @@
+import { View, TouchableOpacity, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/AntDesign'
+
+export function Header({
+  setModalVisible,
+}: {
+  setModalVisible: (visible: boolean) => void
+}) {
+  return (
+    <View className='px-4'>
+      <View className="flex flex-row justify-between mb-4 items-center">
+        <Text className="text-white text-3xl font-semibold">
+          Balanço Mensal
+        </Text>
+
+        <TouchableOpacity onPress={() => setModalVisible(true)}>
+          <Icon name="pluscircle" color={'white'} size={24} />
+        </TouchableOpacity>
+      </View>
+
+      <View className="flex flex-row items-center gap-x-0 mb-1">
+        <View className="flex flex-row items-center">
+          <Icon name="upcircle" size={16} color={'green'} />
+          <Text className="text-green-600 text-lg"> R$350,00</Text>
+        </View>
+
+        <View className="flex flex-row items-center gap-2">
+          <View className="flex flex-row items-center">
+            <Icon name="upcircle" size={12} color={'green'} />
+            <Text className="text-green-500 text-xs"> R$400,00</Text>
+          </View>
+
+          <View className="flex flex-row items-center">
+            <Icon name="downcircle" size={12} color={'red'} />
+            <Text className="text-red-500 text-xs"> R$50,00</Text>
+          </View>
+        </View>
+      </View>
+
+      <View className="flex flex-row gap-x-1 items-center">
+        <Icon name="creditcard" size={16} color={'white'} />
+        <Text className="text-white text-lg">R$238,90</Text>
+      </View>
+    </View>
+  )
+}
