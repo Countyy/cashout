47a4c41b9fb558b3104b764c71bc726612412898
Countyy <joaopedr1234567@gmail.com>
2024-06-25 21:19:16 -0300

    show transaction and creation date in each transaction page

--- app/transaction/[transaction].tsx
+++ app/transaction/[transaction].tsx
@@ -1,5 +1,9 @@
-import { Text, View } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 import { useLocalSearchParams } from 'expo-router'
+import dayjs from 'dayjs'
+import ptbr from 'dayjs/locale/pt-br'
+
+dayjs.locale(ptbr)
 
 export default function Transaction() {
   const local = useLocalSearchParams() as { transaction: string }
@@ -13,12 +17,73 @@ export default function Transaction() {
     pix: 'PIX',
   }
 
-  if (transaction.isDeposit) return
+  const borderColor = transaction.isDeposit
+    ? 'green'
+    : transaction.paymentMethod === 'pix'
+    ? 'royalblue'
+    : transaction.paymentMethod === 'credit-card'
+    ? 'chocolate'
+    : transaction.paymentMethod === 'debit-card'
+    ? 'orangered'
+    : transaction.paymentMethod === 'cash'
+    ? 'mediumturquoise'
+    : 'transparent'
+
+  if (transaction.isDeposit) {
+    return (
+      <View className="py-12 px-4 flex h-screen">
+        <Text
+          className=" text-white text-2xl font-medium mb-1"
+          style={{ color: borderColor }}
+        >
+          Depósito
+        </Text>
+        <Text className=" text-white text-lg mb-8">
+          {Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(transaction.amount)}{' '}
+        </Text>
+        <ScrollView fadingEdgeLength={200}>
+          <Text className="text-white flex-1 text-lg">
+            {transaction.description}
+          </Text>
+        </ScrollView>
+
+        <Text className="text-white text-xs mt-4">
+          {dayjs(transaction.date).format(
+            '[Depósito feito] dddd[,] DD/MM/YYYY[, às] HH:mm[.]'
+          )}
+        </Text>
+      </View>
+    )
+  }
 
   return (
-    <View className="py-12 px-4">
-      <Text className=" text-white text-2xl font-medium mb-1">Compra no {friendlyPaymentMethods[transaction.paymentMethod]}</Text>
-      <Text className=" text-white text-lg">{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(transaction.amount)}</Text>
+    <View className="py-12 px-4 flex h-screen">
+      <Text
+        className=" text-white text-2xl font-medium mb-1"
+        style={{ color: borderColor }}
+      >
+        Compra no {friendlyPaymentMethods[transaction.paymentMethod]}
+      </Text>
+      <Text className=" text-white text-lg mb-8">
+        {Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(transaction.amount)}{' '}
+      </Text>
+      <ScrollView fadingEdgeLength={200}>
+        <Text className="text-white flex-1 text-lg">
+          {transaction.description}
+        </Text>
+      </ScrollView>
+
+      <Text className="text-white text-xs mt-4">
+        {dayjs(transaction.date).format(
+          '[Compra feita] dddd[,] DD/MM/YYYY[, às] HH:mm[.]'
+        )}
+      </Text>
     </View>
   )
 }
