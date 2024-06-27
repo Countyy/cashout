f601b79ad3595dbbbb7c81f39e266bf09ab36968
Countyy <joaopedr1234567@gmail.com>
2024-06-27 00:06:09 -0300

    remove theme provider and add a view with screen dimensions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,21 @@
 import { DarkTheme, ThemeProvider } from '@react-navigation/native'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import { Dimensions, View } from 'react-native'
 
 export default function RootLayout() {
   return (
-    <ThemeProvider value={DarkTheme}>
-      <StatusBar style="inverted" />
-      <Stack screenOptions={{ headerShown: false, animation: 'fade_from_bottom' }} />
-    </ThemeProvider>
+    <View
+      className="p-0 m-0 "
+      style={{
+        height: Dimensions.get('screen').height,
+        width: Dimensions.get('screen').width,
+      }}
+    >
+      <StatusBar style="light" />
+        <Stack
+          screenOptions={{ headerShown: false, animation: 'fade_from_bottom' }}
+        />
+    </View>
   )
 }
