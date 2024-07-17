a0cbc650352f2a716495b2d707d6a2fc05ec0fd7
Countyy <joaopedr1234567@gmail.com>
2024-07-16 22:54:47 -0300

    remove unused imports and change stack animation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import { DarkTheme, ThemeProvider } from '@react-navigation/native'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { Dimensions, View } from 'react-native'
@@ -6,16 +5,16 @@ import { Dimensions, View } from 'react-native'
 export default function RootLayout() {
   return (
     <View
-      className="p-0 m-0 "
+      className="p-0 m-0"
       style={{
         height: Dimensions.get('screen').height,
         width: Dimensions.get('screen').width,
       }}
     >
       <StatusBar style="light" />
-        <Stack
-          screenOptions={{ headerShown: false, animation: 'fade_from_bottom' }}
-        />
+      <Stack
+        screenOptions={{ headerShown: false, animation: 'fade_from_bottom' }}
+      />
     </View>
   )
 }
